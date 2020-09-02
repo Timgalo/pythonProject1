e6027976950f835c76e0eb76cc097cc841313916
Timgalo <67355766+Timgalo@users.noreply.github.com>
2020-09-02 22:09:56 +0800

    Update and rename main.py to unit test.py

---
+++ .github/workflows/unit test.py
@@ -0,0 +1,27 @@
+ from unittest.mock import patch
+
+
+class TestProject(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_Get(self):
+       with patch('project.requests.get') as mocked_get:
+           mocked_get.return_value.ok = True
+           mocked_get.return_value.text = 'Success!'
+
+if __name__ == '__main__':
+    unittest.main()
